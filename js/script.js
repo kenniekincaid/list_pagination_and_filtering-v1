8d48c80e88f3a4838076472ed8b3aaa2401d5faf
Kendra Roberson <kendra.roberson@verizon.com>
2019-06-24 15:55:51 -0500

    Added comments to project
    About to submit project for a grade

--- js/script.js
+++ js/script.js
@@ -1,73 +1,52 @@
+
+//Initial global variables that contain my list and maximum number of entries per page.
 const studentList = document.querySelectorAll('li'); //Global variable for my studentList index
 const itemsPerPage = 10; //The maximum number of entries I want on each page.
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
+//ShowPage function
 const showPage = (studentList, page) => { // assigned my list of students and page to showPage
-   let startIndex = (page * itemsPerPage) - itemsPerPage; 
-   let endIndex = (page * itemsPerPage);
+   let startIndex = (page * itemsPerPage) - itemsPerPage; //determines the first item to be shown on the page
+   let endIndex = (page * itemsPerPage); //determines the last items to be shown on the page
    for (let i = 0; i < studentList.length; i++) { //loop to repeat showing links 
-      if (i >= startIndex && i < endIndex) {
-         studentList[i].style.display = 'block'; 
+      if (i >= startIndex && i < endIndex) { //my condition that determines if page will show
+         studentList[i].style.display = 'block'; //page will show if condition is true
       } else {
-         studentList[i].style.display = 'none';
+         studentList[i].style.display = 'none'; //pay will not show if condition is false
       }
    }
 };
-showPage(studentList, 1);
+showPage(studentList, 1); //calling the function
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-// (54/10 = 5 full pages with a remainder of 4 contained on a 6th page.
-const appendPageLinks = (studentList) => { 
-   let numberOfPages = Math.ceil(studentList.length / itemsPerPage);
-   let div = document.createElement('div');
-   div.className="pagination";
-   let page = document.querySelector('.page');
-   page.appendChild(div);
-   let ul = document.createElement('ul');
-   div.appendChild(ul)
+//Function to create functioning Pagination buttons
+const appendPageLinks = (studentList) => { //variable to append pages to my student list
+   let numberOfPages = Math.ceil(studentList.length / itemsPerPage); //determines how many pages are needed.
+   let div = document.createElement('div'); //assigns the div element to my div variable
+   div.className="pagination"; //assigns div to className pagination
+   let page = document.querySelector('.page'); //assigns class .page to the page variable
+   page.appendChild(div); //appeands div to its parent
+   let ul = document.createElement('ul'); //assigns the ul element to my ul variable
+   div.appendChild(ul)  // appends ul to its parent
    for (let l = 1; l <= numberOfPages; l++) { //loop for page numbers = 1
-      let li = document.createElement('li');
+      let li = document.createElement('li'); 
       let a = document.createElement('a');
-      ul.appendChild(li);
-      li.appendChild(a);
-      a.setAttribute('href', '#');
+      ul.appendChild(li); //appends li to its parent.
+      li.appendChild(a);  //assigns link to li.
+      a.setAttribute('href', '#'); //assigns href contents to a.
       a.textContent=l;
-   if (l === 1) {
-      a.className="active";
+   if (l === 1) { //if page number is exactly 1 in type and value, the page is active.
+      a.className="active"; 
    } 
   
-   a.addEventListener('click', (e) => { //
-      let active = e.target.textContent; // 
+   a.addEventListener('click', (e) => { // whenever a page number is clicked
+      let active = e.target.textContent; //  makes the page active
       let a = document.querySelectorAll('a'); // 
       showPage(studentList, active); // 
-      for (let j = 0; j < a.length; j++) { //
+      for (let j = 0; j < a.length; j++) { // ensures the active page is highlighted
          a[j].classList.remove("active"); //
       }
-      event.target.className="active"; //
+      event.target.className="active"; // targe
    });
    }
 }; 
-showPage(studentList, 1); //
-appendPageLinks(studentList); //
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+showPage(studentList, 1); //call the student list and first page
+appendPageLinks(studentList); //calls the creation of whatever number of 
