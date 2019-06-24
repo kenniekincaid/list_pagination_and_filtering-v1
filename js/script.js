b5883ad56bad11800e989531242a885188af3c4f
Kendra Roberson <kendra.roberson@verizon.com>
2019-06-24 14:48:24 -0500

    Working Codes thus far
    Pages and buttons are working!

--- js/script.js
+++ js/script.js
@@ -1,24 +1,5 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-const studentList = document.querySelectorAll('li');
-const itemsPerPage = 10;
+const studentList = document.querySelectorAll('li'); //Global variable for my studentList index
+const itemsPerPage = 10; //The maximum number of entries I want on each page.
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
@@ -35,12 +16,12 @@ const itemsPerPage = 10;
        "invoke" the function 
 ***/
 
-const showPage = (studentList, page) => {
-   let startIndex = (page * itemsPerPage) - itemsPerPage;
+const showPage = (studentList, page) => { // assigned my list of students and page to showPage
+   let startIndex = (page * itemsPerPage) - itemsPerPage; 
    let endIndex = (page * itemsPerPage);
-   for (let i = 0; i < studentList.length; i++) {
+   for (let i = 0; i < studentList.length; i++) { //loop to repeat showing links 
       if (i >= startIndex && i < endIndex) {
-         studentList[i].style.display = 'block';
+         studentList[i].style.display = 'block'; 
       } else {
          studentList[i].style.display = 'none';
       }
@@ -54,15 +35,38 @@ showPage(studentList, 1);
 ***/
 
 // (54/10 = 5 full pages with a remainder of 4 contained on a 6th page.
-const appendPageLinks = (studentList) => {
-   let page = document.createElement(div);
-   let div = document.createElement(ul);
-   let ul = document.createElement(li);
-   //div.setAttribute('pagination', 'class');
+const appendPageLinks = (studentList) => { 
+   let numberOfPages = Math.ceil(studentList.length / itemsPerPage);
+   let div = document.createElement('div');
+   div.className="pagination";
+   let page = document.querySelector('.page');
    page.appendChild(div);
-   div.appendChild(ul);
-   ul.appendChild(li);
-}
+   let ul = document.createElement('ul');
+   div.appendChild(ul)
+   for (let l = 1; l <= numberOfPages; l++) { //loop for page numbers = 1
+      let li = document.createElement('li');
+      let a = document.createElement('a');
+      ul.appendChild(li);
+      li.appendChild(a);
+      a.setAttribute('href', '#');
+      a.textContent=l;
+   if (l === 1) {
+      a.className="active";
+   } 
+  
+   a.addEventListener('click', (e) => { //
+      let active = e.target.textContent; // 
+      let a = document.querySelectorAll('a'); // 
+      showPage(studentList, active); // 
+      for (let j = 0; j < a.length; j++) { //
+         a[j].classList.remove("active"); //
+      }
+      event.target.className="active"; //
+   });
+   }
+}; 
+showPage(studentList, 1); //
+appendPageLinks(studentList); //
 
 
 
